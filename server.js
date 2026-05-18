2fd2554d7dbd4741d5bad94b67c6af2e430bc0f9
tuhinroy-b3net <tuhin.roy@b3net.org>
2026-05-18 12:30:41 +0530

    chore:
    base url removed for reset password

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,  () => {
-  //console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
   console.log(`✅ Environment: ${process.env.NODE_ENV}`);
 });
